8cdca858104a213289038d2ca7f6749512e4d3d6
Rubentxu <rubentxu74@gmail.com>
2018-05-06 14:45:10 -0700

    renombrado dist a docs

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: '',
-    assetsPublicPath: 'recambios2uso',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
